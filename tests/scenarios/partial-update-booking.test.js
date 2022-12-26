365c79528f88c45b3806689da3f559794a94a1e3
DIOSY PRATAMA <diosypratama.dp@gmail.com>
2022-12-27 00:59:59 +0700

    update partial code

--- tests/scenarios/partial-update-booking.test.js
+++ tests/scenarios/partial-update-booking.test.js
@@ -1,12 +1,13 @@
 import { assert } from "chai";
 import HerokuApi from '../pages/heroku.api';
 import * as data from '../data/user.data';
-import { getParams} from '$helper/lib-api';
+import { getParams} from '../helper/lib-api';
 
 describe('update Booking partial', () =>{
 
     it('Must successfully make a reservation when entering valid data', async () => {
-        const response = await HerokuApi.partialupdatebooking(data.bookingdatapartial);
+        const param = getParams(data.bookingdatapartial);
+        const response = await HerokuApi.booking(param);
 
         assert.equal(response.status, 200);
 
